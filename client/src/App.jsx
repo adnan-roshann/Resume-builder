be9265b1dc4428eb4a9c2bb1e2b7e5b9a8f80c7a
Adnan Roshan <adnanroshan46@gmail.com>
2025-11-12 13:39:26 +0530

    Dashboard created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
+import Dashboard from "./pages/Dashboard";
+
+
 
 const App = () => {
   return (
-    <div>
+    <>
+     <Routes>
+  <Route path='/' element={<Home />} />
+
+  <Route path='app' element={<Layout />}>
+    <Route index element={<Dashboard />} />
+    <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+    
+  </Route>
+
+
+  <Route path='view/:resumeId' element={<Preview />} />
+  <Route path='login' element={<Login />} />
+</Routes>
 
       
-    </div>
+    </>
   )
 }
 
