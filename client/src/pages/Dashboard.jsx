45453d024921f3df280e588b4ff3f7a4bfbaa723
Adnan Roshan <adnanroshan46@gmail.com>
2025-11-12 14:12:32 +0530

    icon added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {PlusIcon, UploadCloudIcon} from 'lucide-react'
+import {FilePenLineIcon, PlusIcon, UploadCloudIcon} from 'lucide-react'
 import { dummyResumeData } from '../assets/assets'
 
 const dashboard = () => {
@@ -46,7 +46,14 @@ const dashboard = () => {
 
               const baseColor = colors[index % colors.length];
               return(
-                <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer'>
+                <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer'
+                style={{background: `linear-gradient(135deg, ${baseColor}10,${baseColor}40)`,borderColor: baseColor + '40'}}>
+
+                  <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{color: baseColor}}/>
+                  <p className='text-sm group-hover:scale-105 transition-all px-2 text-center' style={{color: baseColor}}>{resume.title}</p>
+                  <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center' style={{color: baseColor + '90'}}>
+                    Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+                  </p>
 
                 </button>
               )
