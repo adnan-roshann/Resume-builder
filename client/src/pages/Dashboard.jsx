4e7676190cf025b46f9b8d21b3a20a50fc8e8c7e
Adnan Roshan <adnanroshan46@gmail.com>
2025-11-13 18:29:43 +0530

    upload button updated

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -108,6 +108,28 @@ const dashboard = () => {
             )
           }
 
+          {showUploadResume && (
+
+             <form onSubmit={createResume} onClick={()=> setShowCreateResume(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center
+              '>
+                <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+                  <h2 className='text-xl font-bold mb-4'>
+                    Create a Resume 
+                  </h2>
+                  <input type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required
+                   /> 
+                   <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors
+                   '>Create Resume</button>
+                   <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors
+                   ' onClick={()=>
+                   {setShowCreateResume(false);setTitle('')}
+                   }/>
+                </div>
+              </form>
+
+          )
+          }
+
        </div>
     </div>
   )
